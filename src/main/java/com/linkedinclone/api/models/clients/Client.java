ef5b8b48803864e4141f645040847281572120f7
MedAfkir <afkir_mohamed_01@hotmail.fr>
2023-02-25 22:43:41 +0100

    Add followersLength & followingLength attributes to Client

--- src/main/java/com/linkedinclone/api/models/clients/Client.java
+++ src/main/java/com/linkedinclone/api/models/clients/Client.java
@@ -3,6 +3,8 @@ package com.linkedinclone.api.models.clients;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.linkedinclone.api.models.likes.comment.CommentLike;
 import com.linkedinclone.api.models.likes.post.PostLike;
+import com.linkedinclone.api.models.requests.Request;
+import com.linkedinclone.api.models.requests.RequestState;
 import com.linkedinclone.api.models.roles.Role;
 import com.linkedinclone.api.models.comments.Comment;
 import com.linkedinclone.api.models.experiences.Experience;
@@ -25,6 +27,9 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Client extends User {
+
+    private boolean privateAccount = false;
+
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "position_id")
     @JsonIgnore
@@ -36,15 +41,11 @@ public class Client extends User {
 
     @OneToMany(mappedBy = "client", cascade = CascadeType.ALL)
     @JsonIgnore
-    private List<Post> posts;
-
-    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL)
-    @JsonIgnore
-    private List<Comment> comments;
-
-    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL)
-    @JsonIgnore
     private List<Experience> experiences;
+
+    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Post> posts;
 
     @OneToMany(mappedBy = "client", cascade = CascadeType.ALL)
     @JsonIgnore
@@ -52,9 +53,37 @@ public class Client extends User {
 
     @OneToMany(mappedBy = "client", cascade = CascadeType.ALL)
     @JsonIgnore
+    private List<Comment> comments;
+
+    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL)
+    @JsonIgnore
     private List<CommentLike> commentLikes;
 
-    private boolean privateAccount = false;
+    @OneToMany(mappedBy = "sender")
+    private Collection<Request> requestsSent;
+
+    @OneToMany(mappedBy = "receiver")
+    private Collection<Request> requestsReceived;
+
+    @Transient
+    private int followersLength;
+
+    @Transient
+    private int followingLength;
+
+    public int getFollowersLength() {
+        return requestsSent.stream()
+                .filter(request -> request.getState().equals(RequestState.ACCEPTED))
+                .toList()
+                .size();
+    }
+
+    public int getFollowingLength() {
+        return requestsReceived.stream()
+                .filter(request -> request.getState().equals(RequestState.ACCEPTED))
+                .toList()
+                .size();
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
