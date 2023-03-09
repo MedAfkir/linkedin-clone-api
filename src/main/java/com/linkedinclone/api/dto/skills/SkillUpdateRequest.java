8e794ce69b418a19bf79f1f79ea2784d5126c6ca
MedAfkir <afkir_mohamed_01@hotmail.fr>
2023-03-09 17:01:06 +0100

    remove unused imports

--- src/main/java/com/linkedinclone/api/dto/skills/SkillUpdateRequest.java
+++ src/main/java/com/linkedinclone/api/dto/skills/SkillUpdateRequest.java
@@ -2,7 +2,6 @@ package com.linkedinclone.api.dto.skills;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
